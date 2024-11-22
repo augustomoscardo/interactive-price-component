0b6e55668e95623e9bb8299c7b8487e73a9da82e
Augusto Moscardo <augustomsaluz@gmail.com>
2024-11-22 16:49:46 -0300

    Creating header

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 
 export function App() {
-
   return (
     <div className="h-screen bg-very-pale-blue">
       <div className="bg-pattern bg-no-repeat bg-[length:100%_auto] w-full h-full flex flex-col items-center">
-        <div className="bg-circles bg-no-repeat flex flex-col gap-3">
-          <h1 className="mx-auto">Simple, traffic-based pricing</h1>
-          <p>Sig-up for our 30-day trial. No credit card required</p>
+        <div className="bg-circles bg-no-repeat bg-center flex flex-col gap-3 justify-center mt-12 h-48">
+          <h1 className="mx-auto text-3xl font-bold text-dark-desaturated-blue">
+            Simple, traffic-based pricing
+          </h1>
+          <p className="text-center text-15 font-semibold text-grayish-blue">
+            Sign-up for our 30-day trial. No credit card required.
+          </p>
         </div>
 
         <div className="flex flex-col gap-4">
           <div className="flex items-center justify-between w-[500px]">
-            <span>100k pageviews</span>
-            <span><strong>$16.00</strong>/ month</span>
+            <span className="text-grayish-blue uppercase tracking-widest font-bold text-15">100k pageviews</span>
+            <span className="text-grayish-blue font-semibold"><strong className="text-dark-desaturated-blue text-3xl font-bold">$16.00</strong>/ month</span>
           </div>
 
           <div>
-            <input type="range" name="" id="" />
+            <input className="w-full" type="range" name="" id="" />
           </div>
 
           <div className="flex items-center gap-2">
@@ -40,15 +43,15 @@ export function App() {
               </div>
               <div className="flex items-center gap-2">
                 <img src="" alt="" />
-                <span>Unlimited websites</span>
+                <span>100% data ownership</span>
               </div>
               <div className="flex items-center gap-2">
                 <img src="" alt="" />
-                <span>Unlimited websites</span>
+                <span>Email reports</span>
               </div>
             </div>
 
-            <button>Start my trial</button>
+            <button className="bg-dark-desaturated-blue px-10 py-2 text-white rounded-full hover:brightness-90">Start my trial</button>
           </div>
         </div>
       </div>
